0bf29e650768017e974138adfb55ed6719415914
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2023-12-17 17:50:59 +0700

    sixth commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // random function
 function random (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export default random
